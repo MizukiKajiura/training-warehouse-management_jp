79f398f8a45b03aaa77836417f6a0aba21082603
MizukiKajiura <mizuki_kajiura@1excellence.com>
2025-12-16 14:25:08 +0900

    exerciseの解答を消去

--- beginner/ui/exercise.js
+++ beginner/ui/exercise.js
@@ -4,32 +4,8 @@ async function fetchMenus() {
         throw new Error("Could not fetch menus");
     }
     const menusJson = await menus.json();
-    console.log(menus.ok,"ok");
     console.log(menusJson);
     renderMenus(menusJson);
-}
-
-
-
-
-//ラムダ式　or アロー関数？
-(a) => {引数aを用いた処理}
- 
-var object = {
-
-    name : "name",
-
-    age : 20,
-
-    cal : function(){
-        var result = this.age + 1;
-        return result;
-    }
-}
-
-function test(){
-    alert(object.cal());
-   // alert(object.name);
 }
 
 function renderMenus(menusJson) {
@@ -40,18 +16,14 @@ function renderMenus(menusJson) {
     menus.innerHTML = "";
     const menuTable = document.createElement("table");
     menuTable.innerHTML = "<th>Menu ID</th><th>Menu Name</th>";
-
-    console.log(menusJson,"This is menusJson");
-    console.log(menusJson.menus,"This is menus");
-    
     menusJson.menus.forEach((menu) => {
         menuTable.innerHTML += `
         <tr>
-            <td>${menu.id}</td><td>${menu.name}</td>
+            <td>${menu.id}</td>
+            <td>${menu.name}</td>
         </tr>
         `;
-        menus.appendChild(menuTable)
-    
+        menus.appendChild(menuTable);
     });
 }
 
@@ -64,12 +36,12 @@ async function handleRegisterMenu(event) {
     };
     
     // TODO: "fetch"を完成させる
-    const response = await fetch("http://localhost:8080/menus", {
-        method: "POST",
+    const response = await fetch("", {
+        method: "",
         headers: {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
         },
-        body: JSON.stringify(menu)
+        body: "送信するデータを記述",
     });
     if (!response.ok) {
         const error = await response.json();
